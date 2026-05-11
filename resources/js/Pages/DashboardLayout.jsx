0e81594d9412e6bc035169ed6561492e39431cc1
hanz-fr <raihanf787@gmail.com>
2026-05-11 18:17:08 +0700

    feat: collapsible sidebar; try to use iconify-json/basil

--- resources/js/Pages/DashboardLayout.jsx
+++ resources/js/Pages/DashboardLayout.jsx
@@ -2,12 +2,14 @@ import { Link, Head, usePage } from "@inertiajs/react";
 import { useState } from "react";
 import { TransactionsIcon, HomeIcon, PaymentIcon, ProductsIcon, BoothIcon, BellIcon } from "../Components/Icons";
 import Button from "../Components/Buttons";
-import { Toaster, toast } from 'sonner'
+import { Toaster, toast } from 'sonner';
+import { Icon } from '@iconify/react';
 
 export default function DashboardLayout({ children }) {
     const { url } = usePage();
     const [notifOpen, setNotifOpen] = useState(false);
     const [profileOpen, setProfileOpen] = useState(false);
+    const [collapsed, setCollapsed] = useState(true);
 
     const navItems = [
         { href: "/dashboard", label: "Dashboard", icon: HomeIcon },
@@ -21,21 +23,28 @@ export default function DashboardLayout({ children }) {
         <>
             <Head title="Dashboard" />
             <div className="flex flex-col min-h-screen bg-gray-100">
-                <aside className="fixed top-0 left-0 z-50 h-screen bg-white border-r border-slate-400 flex flex-col lg:z-30 lg:w-64 -translate-x-full lg:translate-x-0">
+                <aside className={`fixed top-0 left-0 z-50 h-screen bg-white border-r border-slate-400 flex flex-col lg:z-30 ${collapsed ? 'lg:w-64' : 'lg:w-24'}  -translate-x-full lg:translate-x-0 transition-all`}>
                     <div className="flex items-center h-16 px-4">
                         <Link href="/" className="flex items-center gap-3">
-                            <div className="w-8 h-8 rounded-lg bg-gradient-to-br from-blue-400 to-blue-600 flex items-center justify-center text-white font-bold text-lg">
-                                T
-                            </div>
-                            <div className="flex flex-col">
-                                <span className="text-xl font-bold text-gray-700">
-                                    Nama Toko
-                                </span>
-                                <span className="text-xs text-gray-500">
-                                    by Tokoku
-                                </span>
-                            </div>
+                            {collapsed && (
+                                <>
+                                    <div className="w-8 h-8 rounded-lg bg-gradient-to-br from-blue-400 to-blue-600 flex items-center justify-center text-white font-bold text-lg">
+                                        T
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <span className="text-xl font-bold text-gray-700">
+                                            Nama Toko
+                                        </span>
+                                        <span className="text-xs text-gray-500">
+                                            by Tokoku
+                                        </span>
+                                    </div>
+                                </>
+                            )}
                         </Link>
+                        <Button onClick={() => setCollapsed(!collapsed)} variant={'ghost'} size={'sm'} className={`w-fit ${collapsed ? 'ml-auto mt-3' : 'mx-auto'} text-black hover:bg-gray-100`}>
+                            <Icon icon={collapsed ? "basil:caret-left-outline" : "basil:menu-outline"} width={24} />
+                        </Button>
                     </div>
                     <nav className="flex-1 overflow-y-auto py-4 px-3">
                         <ul className="space-y-1">
@@ -45,10 +54,10 @@ export default function DashboardLayout({ children }) {
                                     <li key={href}>
                                     <Link
                                         href={href}
-                                        className={`flex items-center gap-3 px-3 py-2.5 rounded-lg transition-all duration-200 font-medium ${isActive ? 'bg-[#DDEBFF] text-[#2B7FFF] hover:bg-[#b7d5ff]' : 'text-gray-700 hover:bg-gray-100'}`}
+                                        className={`flex ${!collapsed && 'justify-center'} items-center gap-3 px-3 py-2.5 rounded-lg transition-all duration-200 font-medium ${isActive ? 'bg-[#DDEBFF] text-[#2B7FFF] hover:bg-[#b7d5ff]' : 'text-gray-700 hover:bg-gray-100'}`}
                                         >
                                         <Icon size="24"/>
-                                        <span>{label}</span>
+                                        {collapsed && <span>{label}</span>}
                                     </Link>
                                 </li>
                                 )
@@ -57,7 +66,7 @@ export default function DashboardLayout({ children }) {
                     </nav>
                 </aside>
 
-                <header className="fixed top-0 right-0 z-20 h-16 bg-white border-b border-gray-200 transition-all duration-300 lg:left-64 left-0">
+                <header className="fixed top-0 right-0 z-20 h-16 bg-white border-b border-gray-200 transition-all duration-300 left-0">
                     <div className="h-full px-4 lg:px-6 flex items-center justify-between gap-4">
                         <Link
                             href="/dashboard"
@@ -143,7 +152,7 @@ export default function DashboardLayout({ children }) {
                     </div>
                 </header>
 
-                <main className="pt-16 pb-16 lg:pb-0 lg:ml-64 flex flex-1 min-h-screen">
+                <main className={`pt-16 pb-16 lg:pb-0 ${collapsed ? 'lg:ml-64' : 'lg:ml-24'} flex flex-1 min-h-screen`}>
                     {children}
                 </main>
 
