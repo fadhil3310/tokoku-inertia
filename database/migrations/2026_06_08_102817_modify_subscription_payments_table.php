bd56021f153b565145eafed66d2d2e25af061658
nanand.r <rafirenan12@gmail.com>
2026-06-08 20:03:35 +0700

    subscription payment table new, pls migrate

--- database/migrations/2026_06_08_102817_modify_subscription_payments_table.php
+++ database/migrations/2026_06_08_102817_modify_subscription_payments_table.php
@@ -6,19 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::table('subscription_payments', function (Blueprint $table) {            
+            $table->foreignId('subscription_id')->constrained('subscriptions')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('plan_id')->constrained('plans')->cascadeOnDelete();
+            
+            $table->enum('payment_method', ['midtrans', 'cash']);
+            $table->integer('amount')->default(1)->comment('Acts as unit quantity');
+            $table->integer('price');
+            $table->integer('grand_total');
+            $table->string('description')->nullable();
+            $table->enum('status', ['pending', 'completed', 'canceled'])->default('pending');
+            $table->string('payment_url', 500)->nullable();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('subscription_payments');
     }
 };
